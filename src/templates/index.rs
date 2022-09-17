45ce40735d40007373c931dc4642255d82dbf286
afidegnum <afidegnum@yahoo.com>
2022-09-17 18:17:42 +0000

    a little issue

--- src/templates/index.rs
+++ src/templates/index.rs
@@ -1,12 +1,13 @@
-use perseus::{make_rx, Html, RenderFnResultWithCause, SsrNode, Template};
+use gloo_timers::future::TimeoutFuture;
+use perseus::{spawn_local_scoped, Html, SsrNode, Template};
 use serde::{Deserialize, Serialize};
 use sycamore::prelude::*;
 
-use gloo_timers::future::TimeoutFuture;
-use rand::Rng;
+// use gloo_timers::future::TimeoutFuture;
+// use rand::Rng;
 use std::error::Error as SysError;
 use std::fmt::Display;
-use sycamore::suspense::{use_transition, Suspense};
+// use sycamore::suspense::{use_transition, Suspense};
 
 // use sycamore::prelude::{view, View};
 
@@ -33,47 +34,102 @@ impl Block {
     }
 }
 
-#[component]
-async fn Child<G: Html>(cx: Scope<'_>, block: Block) -> View<G> {
-    let delay_ms = rand::thread_rng().gen_range(200..500);
-    TimeoutFuture::new(delay_ms).await;
+// #[component]
+// fn Child<G: Html>(cx: Scope<'_>, block: Block) -> View<G> {
+//     let delay_ms = rand::thread_rng().gen_range(200..500);
+//     // TimeoutFuture::new(delay_ms).await;
 
-    let content = if let Ok(content) = block.content::<G>(cx) {
-        view! { cx, (content) }
-    } else {
-        view! { cx, "Oh no!" }
-    };
+//     let content = if let Ok(content) = block.content::<G>(cx) {
+//         view! { cx, (content) }
+//     } else {
+//         view! { cx, "Oh no!" }
+//     };
 
-    view! { cx,
-        div {
-            p { "Content loaded after " (delay_ms) "ms" }
+//     // spawn_local_scoped(cx, async move {
+//     //     TimeoutFuture::new(delay_ms).await;
+//     //     let content = if let Ok(content) = block.content::<G>(cx) {
+//     //         view! { cx, (content) }
+//     //     } else {
+//     //         view! { cx, "Oh no!" }
+//     //     };
+//     // });
 
-            p { (if let Ok(content) = block.content(cx) { view! { cx, (content) } } else { view! { cx, "Oh no!" } }) }
-            p { (content) }
-        }
-    }
-}
+// spawn_local_scoped(cx, async move {
+//     TimeoutFuture::new(delay_ms).await;
+//     view! { cx,
+//         div {
+//             p { (if let Ok(content) = block.content(cx) { view! { cx, (content) } } else { view! { cx, "Oh no!" } }) }
+//         }
+//     }
+// });
+// }
 
 #[perseus::template_rx]
 #[component(IndexPage<G>)]
 pub fn index_page<G: Html>(cx: Scope) -> View<G> {
     let block = create_signal(cx, Block::One);
-    let transition = use_transition(cx);
-    let update = move |x| transition.start(move || block.set(x), || ());
+    // let transition = use_transition(cx);
+    // let update = move |x| transition.start(move || block.set(x), || ());
+    let update = move |x| block.set(x);
+
+    // let content = if let Ok(content) = block.content(cx) {
+    //     view! { cx, (content) }
+    // } else {
+    //     view! { cx, "Oh no!" }
+    // };
+
+    // let new_block = spawn_local_scoped(cx, async move {
+    //     TimeoutFuture::new(delay_ms).await;
+    //     view! { cx,
+    //         div {
+    //             p { (if let Ok(content) = block.get(cx) { view! { cx, (content) } } else { view! { cx, "Oh no!" } }) }
+    //         }
+    //     }
+    // });
+
+    // let get_content = move |x: Block| {
+    //     spawn_local_scoped(cx, async move {
+    //         match x.content().await {
+    //             Ok(cnt) => {}
+    //             Err(e) => log::info!("Could not get wallet info: {}", e),
+    //         }
+    //     });
+    // };
+
+    let get_content = move |x: Block| {
+        spawn_local_scoped(cx, async move {
+            if let Ok(content) = x.content::<G>(cx) {
+                view! { cx, (content) }
+            } else {
+                view! { cx, p {"Oh no!"} }
+            };
+        });
+    };
 
     view! { cx,
         div {
-            p { "Suspense + Transitions" }
-            p { "Transition state: " (transition.is_pending().then_some("pending").unwrap_or("done")) }
+
+            // p { "Transition state: " (transition.is_pending().then_some("pending").unwrap_or("done")) }
             button(on:click=move |_| update(Block::One)) { "One" }
             button(on:click=move |_| update(Block::Two)) { "Two" }
             button(on:click=move |_| update(Block::Three)) { "Three" }
-            Suspense(fallback=view! { cx, p { "Loading..." } }) {
-                ({
-                    let block = *block.get();
-                    view! { cx, Child(block) }
-                })
-            }
+            // p { (if let Ok(content) = block.get() { view! { cx, (content) } } else { view! { cx, "Oh no!" } }) }
+
+
+            div{(&get_content(*block.get())) }
+
+
+
+
+            // Suspense(fallback=view! { cx, p { "Loading..." } }) {
+            //     ({
+            //         let block = *block.get();
+            //         view! {
+            //             cx, Child(block)
+            //                                 p { (if let Ok(content) = block.content(cx) { view! { cx, (content) } } else { view! { cx, "Oh no!" } }) }
+            //         }
+            //     })
+            // }
         }
     }
 }
